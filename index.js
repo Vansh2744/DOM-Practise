79a17cbe01b50cf40844a7a6643380519049ecd9
Vansh <vanshchoudhary2744@gmail.com>
2026-09-21 01:19:06 +0530

    dom manipulation for changing the background color

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const button = document.getElementById
+let button = document.getElementById("colorChangeBtn");
+let changePara = document.getElementById("welcome");
+
+button.addEventListener("click", () => {
+  changePara.style.backgroundColor = "red";
+});
