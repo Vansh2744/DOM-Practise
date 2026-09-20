b14168710c93ce58d68ad0f5c66661cede723307
Vansh <vanshchoudhary2744@gmail.com>
2026-09-21 01:52:22 +0530

    Change the text

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ let changePara = document.getElementById("welcome");
 button.addEventListener("click", () => {
   changePara.style.backgroundColor = "red";
 });
+
+document.getElementById("changeText").addEventListener("click", () => {
+  document.getElementById("drag").children[0].textContent = "My First Child";
+});
