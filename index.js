f84fc2080178cf400725b3dcd072894c66498a9d
Vansh <vanshchoudhary2744@gmail.com>
2026-09-21 11:49:41 +0530

    getting input field values

--- index.js
+++ index.js
@@ -26,3 +26,23 @@ document.getElementById("rmLast").addEventListener("click", () => {
 document.getElementsByClassName("todo")[0].addEventListener("click", (e) => {
   console.log(`Target Item : ${e.target.textContent}`);
 });
+
+document.getElementsByClassName("outer")[0].addEventListener("click", () => {
+  console.log("Outer Box");
+});
+
+document
+  .getElementsByClassName("inner-outer")[0]
+  .addEventListener("click", () => {
+    console.log("Inner-Outer Box");
+  });
+
+document.getElementsByClassName("inner")[0].addEventListener("click", () => {
+  console.log("Inner Box");
+});
+
+document.getElementById("submitForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(document.getElementById("nameField").value);
+  console.log(document.getElementById("emailField").value);
+});
